06582f220df4ff0f0a49164ce1cfd7f1591751ee
mirzaghaderi <gh.mirzaghaderi@uok.ac.ir>
2024-03-30 20:50:03 +0330

    Update qpcrTTEST.r

--- R/qpcrTTEST.r
+++ R/qpcrTTEST.r
@@ -12,7 +12,7 @@
 #' @param x a data frame of 4 columns including Conditions, E (efficiency), Gene and Ct values (see example below). Biological replicates needs to be equal for all Genes. Each Ct value is the mean of technical replicates. Complete amplification efficiencies of 2 is assumed here for all wells but the calculated efficienies can be used instead.
 #' @param paired a logical indicating whether you want a paired t-test.
 #' @param var.equal a logical variable indicating whether to treat the two variances as being equal. If TRUE then the pooled variance is used to estimate the variance otherwise the Welch (or Satterthwaite) approximation to the degrees of freedom is used.
-#' @param NumberOfrefGenes number of reference genes. Up to two reference genes can be handled.
+#' @param numberOfrefGenes number of reference genes. Up to two reference genes can be handled.
 #' @return A list of two elements:
 #' \describe{
 #'   \item{Row_data}{The row data including Genes and weighed delta Ct (wDCt) values.}

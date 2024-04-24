35652c0c8e78b294963057e7a8f8e7ca3371e994
mirzaghaderi <gh.mirzaghaderi@uok.ac.ir>
2024-04-24 07:58:15 +0330

    Update qpcrANCOVA.r

--- R/qpcrANCOVA.r
+++ R/qpcrANCOVA.r
@@ -37,10 +37,11 @@
 #' @param numberOfrefGenes number of reference genes which is 1 or 2 (Up to two reference genes can be handled).
 #' @param analysisType should be one of "ancova" or "anova".
 #' @param mainFactor.column the factor for which FC is calculated for its levels. The remaining factors (if any) are considered as covariate(s).
-#' @param mainFactor.level.order  a vector of main factor level names. The first level in the vector is used 
+#' @param mainFactor.level.order  NULL or a vector of main factor level names. The first level in the vector is used 
 #' as reference or calibrator which is the reference level or sample that all others are compared to. Examples are untreated 
 #' of time 0. The FC value of the reference or calibrator level is 1 because it is not changed compared to itself.
-#' @param width a positive number determining bar width.
+#' If NULL, the first level of the main factor column is used as calibrator.
+#' @param width a positive number determining bar width. 
 #' @param fill  specify the fill color for the columns in the bar plot. If a vector of two colors is specified, the reference level is differentialy colored.
 #' @param y.axis.adjust  a negative or positive value for reducing or increasing the length of the y axis.
 #' @param letter.position.adjust adjust the distance between the signs and the error bars.

86212e8ced5519d39b863765e1e099018fd5568d
mirzaghaderi <gh.mirzaghaderi@uok.ac.ir>
2026-09-14 23:59:49 +0330

    Update qpcrhlpr.r

--- R/qpcrhlpr.r
+++ R/qpcrhlpr.r
@@ -395,7 +395,7 @@
     }
     
     
-    gene_df <- compute_wDCt(gene_df, numOfFactors, numberOfrefGenes, block,
+    gene_df <- .compute_wDCt(gene_df, numOfFactors, numberOfrefGenes, block,
                             set_missing_target_Ct_to_40 = set_missing_target_Ct_to_40)
     
     gene_df[] <- lapply(gene_df, function(x) {
